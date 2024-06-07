1cd4c1d16e08eeb2648a6c53e47ce32278bf9fe1
facundo-rubino <facundorubino21@gmail.com>
2024-06-07 09:33:04 -0300

    feat: added new entities

---
+++ BusinessLogic/Entidades/MovimientoStock.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using BussinessLogic.Entidades;
+
+namespace BusinessLogic.Entidades
+{
+    public class MovimientoStock
+    {
+        public int Id { get; private set; }
+        public DateTime FechaHora { get; set; }
+        public Articulo Articulo { get; set; }
+        public int Cant { get; set; }
+
+        public MovimientoStock()
+        {
+        }
+    }
+}
+
